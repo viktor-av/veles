a98cf336b3532ab8f37890ee1fe168ff860bef63
Victor Voronin <viktor.voronin@evologics.de>
2025-08-30 11:23:07 +0200

    Bump cpp/msgpack version to 2.1.5

--- cmake/msgpack.cmake
+++ cmake/msgpack.cmake
@@ -1,8 +1,8 @@
 if(NOT MSGPACK_INCLUDE_PATH)
-  set(MSGPACK_URL "https://github.com/msgpack/msgpack-c/releases/download/cpp-2.1.1/msgpack-2.1.1.tar.gz")
-  set(MSGPACK_PATH "${CMAKE_CURRENT_BINARY_DIR}/msgpack-2.1.1.tar.gz")
-  set(MSGPACK_EXTRACT_PATH "${CMAKE_CURRENT_BINARY_DIR}/msgpack-2.1.1")
-  set(MSGPACK_SHA256 "fce702408f0d228a1b9dcab69590d6a94d3938f694b95c9e5e6249617e98d83f")
+  set(MSGPACK_URL "https://github.com/msgpack/msgpack-c/releases/download/cpp-2.1.5/msgpack-2.1.5.tar.gz")
+  set(MSGPACK_PATH "${CMAKE_CURRENT_BINARY_DIR}/msgpack-2.1.5.tar.gz")
+  set(MSGPACK_EXTRACT_PATH "${CMAKE_CURRENT_BINARY_DIR}/msgpack-2.1.5")
+  set(MSGPACK_SHA256 "6126375af9b204611b9d9f154929f4f747e4599e6ae8443b337915dcf2899d2b")
 
   file(DOWNLOAD ${MSGPACK_URL} ${MSGPACK_PATH} EXPECTED_HASH SHA256=${MSGPACK_SHA256})
 
